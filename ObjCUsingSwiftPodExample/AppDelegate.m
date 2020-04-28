fcf2ecec9c877c566c51315b7117f8b5d7a8529c
Massimiliano Ziccardi <mziccard@redhat.com>
2020-04-28 22:49:52 +0200

    First commit

--- ObjCUsingSwiftPodExample/AppDelegate.m
+++ ObjCUsingSwiftPodExample/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AeroGearPush/AeroGearPush-Swift.h>
 
 @interface AppDelegate ()
 
---
+++ ObjCUsingSwiftPodExample/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  ObjCUsingSwiftPodExample
+//
+//  Created by Massimiliano Ziccardi on 28/04/2020.
+//  Copyright © 2020 ziccardi. All rights reserved.
+//
+
+import Foundation
